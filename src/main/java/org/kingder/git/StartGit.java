4bb8af2120038326656f314e49f94a41e6016d69
awswt <389897490@qq.com>
2020-11-23 19:51:46 +0800

    提交git测试

---
+++ src/main/java/org/kingder/git/StartGit.java
@@ -0,0 +1,9 @@
+package org.kingder.git;
+
+/**
+ * @author: kinder
+ * @time: 2020-11-23 19:51
+ * @description: the task of this class
+ */
+public class StartGit {
+}
---
+++ src/main/java/org/kingder/git/TestGit.java
@@ -0,0 +1,9 @@
+package org.kingder.git;
+
+/**
+ * @author: kinder
+ * @time: 2020-11-23 19:50
+ * @description: the task of this class
+ */
+public class TestGit {
+}
